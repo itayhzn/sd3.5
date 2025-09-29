b7a0a79855a39876bc5fd1d20227b9d6e3d807e7
itayhzn <itay.hzn@gmail.com>
2025-09-30 01:43:24 +0300

    bugfix timesteps between 0 and 1 (float)

--- sd3_impls.py
+++ sd3_impls.py
@@ -242,7 +242,8 @@ class CFGDenoiser(torch.nn.Module):
             # Per-pixel saliency: ||∂L/∂x|| over channels
             g = x_leaf.grad
 
-            current_timestep = f'{int(timestep[0].item()):03d}'
+            current_timestep = f'{int(timestep[0].item()*1000):03d}'
+            
             save_tensors(save_tensors_path, {
                 f"x_t={current_timestep}": x_leaf[0].detach().cpu(),
                 f"pos_out_t={current_timestep}": pos_out[0].detach().cpu(),
