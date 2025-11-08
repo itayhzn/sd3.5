7be8cc4abb583d71f086607c884a0c537b988361
itayhzn <itay.hzn@gmail.com>
2025-11-08 12:11:15 +0200

    bugfix

--- knn_reward.py
+++ knn_reward.py
@@ -11,6 +11,9 @@ class KNNReward:
     def __call__(self, prompt: str, img) -> float:
         z = self.idx.embed_image(img)        # (1, D)
         D, I = self.idx.img_index.search(z, self.k)
-        sim = float(D[0].mean())                 # cosine sim ∈ [-1,1]
-        
+        cossim = float(D[0].mean())                 # cosine sim ∈ [-1,1]
+        theta = np.arccos(np.clip(cossim, -1.0, 1.0))  # angle in [0, π]
+        sim = 1.0 - (theta / np.pi)              # normalize to [0, 1]
+        sim = np.exp(3 * sim) - 1.0              # sharpen
+        sim = np.clip(sim, 0.0, 1.0)              # ensure sim is in [0, 1]
         return sim
